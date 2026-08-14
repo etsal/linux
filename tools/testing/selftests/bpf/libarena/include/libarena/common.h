6b8b1cde9c05130e031a4cca2d455a5b6f2b3174
Emil Tsalapatis <emil@etsalapatis.com>
2026-08-14 14:02:10 -0700

    selftests/bpf: libarena: Add calloc() call
    The arena memory allocator currently does not provide the
    option to zero-initialize the allocated memory. Provide
    a calloc() operation that calls memset() on the returned
    memory. Reuse naive memset implementation already present
    in the arena ASAN code for now. Subsequent patches will
    optimize the function.
    
    Signed-off-by: Emil Tsalapatis <emil@etsalapatis.com>

--- tools/testing/selftests/bpf/libarena/include/libarena/common.h
+++ tools/testing/selftests/bpf/libarena/include/libarena/common.h
@@ -61,6 +61,23 @@ void arena_free(void __arena *ptr);
  */
 #define arena_subprog_init() do { asm volatile ("" :: "r"(&arena)); } while (0)
 
+/*
+ * BPF does not currently support the memset intrinsics. for large
+ * sequential copies, or assignments of large data structures,
+ * the frontend will generate an intrinsic that causes the BPF
+ * backend to exit due to a missing implementation. Provide
+ * implementations for the intrinsic.
+ */
+static inline int arena_memset(s8 __arena *dst, s8 val, size_t size)
+{
+	size_t i;
+
+	for (i = zero; i < size && can_loop; i++)
+		dst[i] = val;
+
+	return 0;
+}
+
 #else /* ! __BPF__ */
 
 #include <stdint.h>
